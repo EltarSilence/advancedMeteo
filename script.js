48790da721fc86f691477dcef196aed54ab47bbb
Giovanni Rizza <eltarsilence@gmail.com>
2019-04-21 22:06:47 +0200

    TEQ + HeightCum + FSI (bootstrap & Jquery optimized)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function clcTEQ() {
-	var tmp = document.getElementById("temp").value;
-	var dwp = document.getElementById("dewp").value;
-	var prs = document.getElementById("press").value;
+	var tmp = $("#temp").val();
+	var dwp = $("#dewp").val();
+	var prs = $("#press").val();
 
 	const A = 19.0785;
 	const B = 4098.025
@@ -9,6 +9,39 @@ function clcTEQ() {
 	const F = 18/28.96;
 
 	var teqK = 7.4131 * Math.pow((1/Number(prs)), 0.29) * ((2480*Math.pow(Math.E, (A - B / (C + Number(dwp))))) / ( Number(prs) - Math.pow(Math.E, (A - B / (C + Number(dwp))) )) + Number(tmp) + 273.15);
-	document.getElementById('resultK').innerHTML = Math.round(teqK * 1000) / 1000;
-	document.getElementById('resultC').innerHTML = Math.round((teqK - 273.15) * 1000) / 1000;
+	$('#resultK').html(Math.round(teqK * 1000) / 1000);
+	$('#resultC').html(Math.round((teqK - 273.15) * 1000) / 1000);
+}
+
+function clcHnC() {
+	var tmp = $("#temp1").val();
+	var dwp = $("#dewp1").val();
+
+	var height = 125 * (Number(tmp) - Number(dwp));
+	var height_feet = height * 3.28;
+	$('#result1h').html(height + ' metri (' + height_feet + ' ft)');
+}
+
+function clcFogSI() {
+	var ts = $("#temp2").val();
+	var t850 = $("#t850").val();
+	var tds = $("#tds").val();
+	var vv850 = $("#vv850").val();
+
+	var color, rischio;
+	var fsi = 4*Number(ts) - 2* (Number(t850) + Number(tds)) + Number(vv850);
+	if (fsi < 31){
+		color = 'red';
+		rischio = 'Elevato';
+	}
+	if (fsi >= 31 && fsi < 55){
+		color = 'yellow';
+		rischio = 'Moderato';
+	}
+	if (fsi >= 55){
+		color = 'green';
+		rischio = 'Basso';
+	}
+	$('#fsi').html(fsi + '<br> (rischio '+rischio+')');
+	$('#fsi').addClass(color);
 }
