55e04ab3d34cd0a175e222446ed453d5ac57a8c5
Eltar <eltarsilence@gmail.com>
2019-06-30 12:30:21 +0200

    120s refresh + DB recording

--- core.php
+++ core.php
@@ -1,5 +1,9 @@
 <?php
 
+  function avg($a, $b){
+    return ($a+$b)/2;
+  }
+
   function getHeatInfo($hi){
     //input heat index
     $arr = [];
@@ -55,7 +59,6 @@
   function getColorUV($uv){
 
     $arr = [];
-
     if ($uv < 1){
       $arr['color'] = '#a4a8a3';
       $arr['msg'] = 'Intensità dei raggi solari quasi nulla, nessun danno alla pelle, esposizione 100% sicura';
@@ -79,6 +82,11 @@
     if ($uv >=9){
       $arr['color'] = '#ff00d4';
       $arr['msg'] = 'Intensità dei raggi solari estrema.';
+    }
+
+    if ($uv == 'N/D'){
+      $arr['color'] = '';
+      $arr['msg'] = 'Non disponibile';
     }
 
     return $arr;
@@ -120,25 +128,97 @@
   function getOzono($oz){
     $arr = [];
     if ($oz <= 80){
-      $arr['msg'] = 'Molto basso';
+      $arr['msg'] = 'Molto bassa';
       $arr['color'] = '#1df2dd';
     }
     if ($oz>80 && $oz<120){
-      $arr['msg'] = 'Basso';
+      $arr['msg'] = 'Bassa';
       $arr['color'] = '#42fc77';
     }
     if ($oz>=120 && $oz<180){
-      $arr['msg'] = 'Medio';
+      $arr['msg'] = 'Media';
       $arr['color'] = '#cccf1b';
     }
     if ($oz>=180 && $oz<240){
-      $arr['msg'] = 'Alto';
+      $arr['msg'] = 'Alta';
       $arr['color'] = '#f58f0a';
     }
     if ($oz>=240){
-      $arr['msg'] = 'Molto alto';
+      $arr['msg'] = 'Molta alta';
       $arr['color'] = '#ff1e00';
     }
     return $arr;
   }
+
+  function getBeaufort($kmh){
+    $arr = [];
+    if ($kmh < 1){
+      $arr['lvl'] = 0;
+      $arr['descr'] = 'Calma di vento';
+      $arr['lago'] = 'Calmo';
+    }
+    if ($kmh >=1 && $kmh < 6){
+      $arr['lvl'] = 1;
+      $arr['descr'] = 'Bava di vento';
+      $arr['lago'] = 'Quasi calmo';
+    }
+    if ($kmh >=6 && $kmh < 11){
+      $arr['lvl'] = 2;
+      $arr['descr'] = 'Brezza leggera';
+      $arr['lago'] = 'Poco mosso';
+    }
+    if ($kmh >=11 && $kmh < 19){
+      $arr['lvl'] = 3;
+      $arr['descr'] = 'Brezza tesa';
+      $arr['lago'] = 'Mosso';
+    }
+    if ($kmh >=19 && $kmh < 29){
+      $arr['lvl'] = 4;
+      $arr['descr'] = 'Vento moderato';
+      $arr['lago'] = 'Mosso';
+    }
+    if ($kmh >=29 && $kmh < 39){
+      $arr['lvl'] = 5;
+      $arr['descr'] = 'Vento teso';
+      $arr['lago'] = 'Molto mosso';
+    }
+    if ($kmh >=39 && $kmh < 50){
+      $arr['lvl'] = 6;
+      $arr['descr'] = 'Vento fresco';
+      $arr['lago'] = 'Agitato';
+    }
+    if ($kmh >=50 && $kmh < 62){
+      $arr['lvl'] = 7;
+      $arr['descr'] = 'Vento forte';
+      $arr['lago'] = 'Agitato';
+    }
+    if ($kmh >=62 && $kmh < 75){
+      $arr['lvl'] = 8;
+      $arr['descr'] = 'Burrasca';
+      $arr['lago'] = 'Molto agitato';
+    }
+    if ($kmh >=75 && $kmh < 87){
+      $arr['lvl'] = 9;
+      $arr['descr'] = 'Burrasca forte';
+      $arr['lago'] = 'Grosso';
+    }
+    if ($kmh >=87 && $kmh < 102){
+      $arr['lvl'] = 10;
+      $arr['descr'] = 'Tempesta';
+      $arr['lago'] = 'Molto grosso';
+    }
+    if ($kmh >=102 && $kmh < 117){
+      $arr['lvl'] = 11;
+      $arr['descr'] = 'Fortunale';
+      $arr['lago'] = 'Molto grosso';
+    }
+    if ($kmh >= 117){
+      $arr['lvl'] = 12;
+      $arr['descr'] = 'Uragano';
+      $arr['lago'] = 'Tempestoso';
+    }
+
+    return $arr;
+  }
+
 ?>
--- current.php
+++ current.php
@@ -1,51 +1,85 @@
 <?php
+//error_reporting(0);
 require 'core.php';
 //CURRENT OBS
 $url = 'https://api.weather.com/v2/pws/observations/current?stationId=IGARGNAN4&format=json&units=m&apiKey=81f69e3da6b04689b69e3da6b096898b';
-$json = file_get_contents($url);
-$obj = json_decode($json);
-
-//api retrieval
-$wx = $obj->observations[0];
-
-$sw = $wx->softwareType;
-$nome = $wx->neighborhood;
-$obstime = $wx->obsTimeLocal;
-
-$wm2 = $wx->solarRadiation;
-$uv = $wx->uv;
-$winddir = $wx->winddir;
-$humidity = $wx->humidity;
-
-$current = $wx->metric;
-
-$temp = $current->temp;
-$hi = $current->heatIndex;
-$dewpoint = $current->dewpt;
-$wspd = $current->windSpeed;
-$gusts = $current->windGust;
-$press = $current->pressure;
-
-$calore = getHeatInfo($hi);
-
-//upcoming
-
 $url1 = 'https://api.weatherbit.io/v2.0/forecast/hourly?city=Gargnano&country=IT&key=607265bbae2d428d902a71395af79635&hours=24';
-$txt = file_get_contents($url1);
-$json_fcst = json_decode($txt, true);
-
-//qualita Aria
 $url2 = 'https://api.weatherbit.io/v2.0/current/airquality?city=Gargnano&country=IT&key=607265bbae2d428d902a71395af79635';
-$txt = file_get_contents($url2);
-$json_q = json_decode($txt, true);
 
+$RECORD_ON_DATABASE = true;
+
+try {
+  $json = file_get_contents($url);
+  $obj = json_decode($json);
+
+  //api retrieval
+  $wx = $obj->observations[0];
+
+  $sw = $wx->softwareType;
+  $nome = $wx->neighborhood;
+  $obstime = $wx->obsTimeLocal;
+
+  $wm2 = $wx->solarRadiation;
+  if (is_null($wm2)) {
+    $wm2 = 'N/D';
+    $lux = 'N/D';
+  }
+  else {
+    $lux = floor(convertWm2toLux($wm2));
+  }
+  $uv = $wx->uv;
+  if (is_null($uv)) $uv = 'N/D';
+  $winddir = $wx->winddir;
+  if (is_null($winddir)) $winddir = 'N/D';
+  $humidity = $wx->humidity;
+  if (is_null($humidity)) $humidity = 'N/D';
+
+  $current = $wx->metric;
+
+  $temp = $current->temp;
+  if (is_null($temp)) $temp = 'N/D';
+  $hi = $current->heatIndex;
+  if (is_null($hi)){
+    $hi = 'N/D';
+    $calore = 'N/D';
+  }
+  else {
+    $calore = getHeatInfo($hi);
+  }
+  $dewpoint = $current->dewpt;
+  if (is_null($dewpoint)) $dewpoint = 'N/D';
+  $wspd = $current->windSpeed;
+  if (is_null($wspd)) $wspd = 'N/D';
+  $gusts = $current->windGust;
+  if (is_null($gusts)) $gusts = 'N/D';
+  $beaufort = getBeaufort(avg($wspd, $gusts))['lvl'];
+
+  $press = $current->pressure;
+  if (is_null($press)) $press = 'N/D';
+
+  $qnh = convertQFEToQNH($press);
+
+  $mmh = NULL;
+
+  //upcoming
+  $txt = file_get_contents($url1);
+  $json_fcst = json_decode($txt, true);
+
+  //qualita Aria
+  $txt = file_get_contents($url2);
+  $json_q = json_decode($txt, true);
+}
+catch(Exception $e) {
+  echo 'Errore: ' .$e->getMessage();
+}
 ?>
 
 <html lang="it" dir="ltr">
 <head>
   <meta charset="utf-8">
   <title>Wx Indexes</title>
-  <meta http-equiv="refresh" content="60" >
+  <meta http-equiv="refresh" content="120">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -70,20 +104,18 @@ $json_q = json_decode($txt, true);
             <td><?= $dewpoint ?> °C</td>
           </tr>
           <tr>
-            <td>Indice di calore</td>
+            <td>Indice di calore<br><small>(Temperatura percepita)</small></td>
             <td style="background-color: <?php echo $calore['color'] ?>;">
               + <?= $hi ?> °C<br>
-              <?php echo $calore['msg'] ?><br>
+              <small><?php echo $calore['msg'] ?></small><br>
               <?php echo $humidity?>% di umidit&agrave;
             </td>
           </tr>
-          <tr>
-            <td>Pressione di vapore</td>
-            <td>Satura: <?php echo getVapPressure($temp) ?> mbar<br>
-              Effettiva: <?php echo getVapPressure($dewpoint) ?> mbar</td>
-            </tr>
             <tr>
-              <td>Vento</td>
+              <td>Vento<br>
+                Beaufort: <?php echo '<b>'.$beaufort.'</b> <small>('.getBeaufort(avg($wspd, $gusts))['descr'].')</small>'; ?><br>
+                <?php echo 'Lago: <b>'.getBeaufort(avg($wspd, $gusts))['lago'].'</b>'; ?>
+              </td>
               <td>da <?php echo ceil($winddir / 10) * 10; ?>°<br>
                 min. <?php echo $wspd ?> km/h<br>
                 max. <?php echo $gusts ?> km/h
@@ -92,9 +124,14 @@ $json_q = json_decode($txt, true);
             <tr>
               <td>Pressione Atmosferica</td>
               <td>QFE - <?php echo floor($press) ?> hPa<br>
-                QNH - <?php echo convertQFEToQNH($press) ?> hPa
+                QNH - <?php echo $qnh ?> hPa
               </td>
             </tr>
+            <tr>
+              <td>Pressione di vapore</td>
+              <td>Satura: <?php echo getVapPressure($temp) ?> mbar<br>
+                Effettiva: <?php echo getVapPressure($dewpoint) ?> mbar</td>
+              </tr>
           </table>
         </div>
 
@@ -113,97 +150,197 @@ $json_q = json_decode($txt, true);
             </tr>
             <tr>
               <td>Indice UV</td>
-              <td style="background-color: <?php echo getColorUV($uv)['color']; ?>; color: black"><b><?php echo $uv ?></b><br>
+              <td style="background-color: <?php echo getColorUV($uv)['color']; ?>; color: black"><b><?php echo $uv; ?></b><br>
                 <small><?php echo getColorUV($uv)['msg']; ?></small>
               </td>
             </tr>
             <tr>
               <td>Energia Solare attuale</td>
-              <td><?php echo round($wm2/1000, 2) ?> KWh/mq<br>(<?php echo floor(convertWm2toLux($wm2)); ?> lux @ 555 nm)</td>
+              <td><?php echo round($wm2, 2) ?> Wh/mq<br><small>(<?php echo $lux; ?> lux @ spettro 555 nm)</small></td>
             </tr>
           </table>
         </div>
 
+        <?php
+        $o3 = $json_q['data'][0]['o3'];
+        $pm25 = $json_q['data'][0]['pm25'];
+        $pm10 = $json_q['data'][0]['pm10'];
+        $no2 = $json_q['data'][0]['no2'];
+        $co = $json_q['data'][0]['co'];
+
+        ?>
         <div class="col-md-4">
           <table class="table table-dark">
             <tr>
               <td class="title" colspan="2">Qualit&agrave; dell'atmosfera</td>
             </tr>
             <tr>
-              <td>O<sub>3</sub> (Ozono)</td><td style="background-color: <?php echo getOzono($json_q['data'][0]['o3'])['color'] ?>;"><?php echo floor($json_q['data'][0]['o3']); ?> µg/m<sup>3</sup><br>
-                (<?php echo getOzono($json_q['data'][0]['o3'])['msg'] ?>)</td>
-            </tr>
-            <tr>
-              <td>PM2.5<br><small>(pericolo >25µg/m<sup>3</sup>)</small></td> <td><?php echo floor($json_q['data'][0]['pm25']); ?> µg/m<sup>3</sup></td>
-            </tr>
-            <tr>
-              <td>PM10<br><small>(pericolo >50µg/m<sup>3</sup>)</small></td> <td><?php echo floor($json_q['data'][0]['pm10']); ?> µg/m<sup>3</sup></td>
-            </tr>
-            <tr>
-              <td>NO<sub>2</sub><br><small>(pericolo >35µg/m<sup>3</sup>)</small></td> <td><?php echo round($json_q['data'][0]['no2'],2); ?> µg/m<sup>3</sup></td>
-            </tr>
-            <tr>
-              <td>CO<br><small>(pericolo >10ppm)</small></td> <td><?php echo round(($json_q['data'][0]['co'])/1000); ?> ppm</td>
-            </tr>
-            </table>
-        </div>
-
-          <!--<div class="col-md-4">
-            <table class="table table-dark">
-              <tr>
-                <td class="title">
-                  RADAR Valeggio sul Mincio<hr>
-                  <img width="500px" height="500px" src="http://www.arpa.veneto.it/previsioni/radar_valeggio/PPI_36_Z_2_01.PNG">
-                </td>
+              <td>O<sub>3</sub> (Ozono)</td><td style="background-color: <?php echo getOzono($o3)['color'] ?>;"><?php echo floor($o3); ?> µg/m<sup>3</sup><br>
+                <small>(Quantit&agrave; <?php echo getOzono($o3)['msg'] ?>)</small></td>
               </tr>
               <tr>
-              <td class="title">
-              Webcam Bogliaco<hr>
-              <img width="500px" height="500px" src="http://www.marinadibogliaco.com/meteo/bogliaco.jpg?n=1678601804">
-            </td>
-          </tr>
-        </table>
-      </div>-->
-    </div>
+                <td>PM2.5<br><small>(pericolo >25µg/m<sup>3</sup>)</small></td> <td><?php echo floor($pm25); ?> µg/m<sup>3</sup></td>
+              </tr>
+              <tr>
+                <td>PM10<br><small>(pericolo >50µg/m<sup>3</sup>)</small></td> <td><?php echo floor($pm10); ?> µg/m<sup>3</sup></td>
+              </tr>
+              <tr>
+                <td>NO<sub>2</sub><br><small>(pericolo >35µg/m<sup>3</sup>)</small></td> <td><?php echo round($no2,2); ?> µg/m<sup>3</sup></td>
+              </tr>
+              <tr>
+                <td>CO<br><small>(pericolo >10ppm)</small></td> <td><?php echo round(($co)/1000); ?> ppm</td>
+              </tr>
+            </table>
+          </div>
 
-    <div class="row">
-      <div class="col-md-12">
-        <table class="table table-dark">
+          <!--<div class="col-md-4">
+          <table class="table table-dark">
           <tr>
-            <td class="title" colspan="9">Prossime 8 ore</td>
-          </tr>
-          <tr> <td>Ora</td>
-            <?php
-            for($i=0;$i<8;$i++){
-              echo "<td>Ore ".substr(explode("T", $json_fcst['data'][$i]['timestamp_local'])[1], 0, 2)."</td>";
-            }
-            ?>
-          </tr>
-          <tr> <td>% precipit.</td>
-            <?php
-            for($i=0;$i<8;$i++){
-              echo '<td style="background-color: '.getColorPop($json_fcst['data'][$i]['pop']).';">'.$json_fcst['data'][$i]['pop'].'%</td>';
-            }
-            ?>
-          </tr>
-          <tr> <td>Clima</td>
-            <?php
-            for($i=0;$i<8;$i++){
-              echo '<td><img width="50px" height="50px" src="https://www.weatherbit.io/static/img/icons/'.$json_fcst['data'][$i]['weather']['icon'].'.png"></td>';
-            }
-            ?>
-          </tr>
-          <tr> <td>max. Okta</td>
-            <?php
-            for($i=0;$i<8;$i++){
-              echo '<td>'.round((($json_fcst['data'][$i]['clouds_mid'])/100*8)).'</td>';
-            }
-            ?>
-          </tr>
-        </table>
-      </div>
-    </div>
+          <td class="title">
+          RADAR Valeggio sul Mincio<hr>
+          <img width="500px" height="500px" src="http://www.arpa.veneto.it/previsioni/radar_valeggio/PPI_36_Z_2_01.PNG">
+        </td>
+      </tr>
+      <tr>
+      <td class="title">
+      Webcam Bogliaco<hr>
+      <img width="500px" height="500px" src="http://www.marinadibogliaco.com/meteo/bogliaco.jpg?n=1678601804">
+    </td>
+  </tr>
+</table>
+</div>-->
 </div>
+
+<div class="row">
+  <div class="col-md-12">
+    <table class="table table-dark">
+      <tr>
+        <td class="title" colspan="9">Prossime 8 ore</td>
+      </tr>
+      <tr> <td>Ora</td>
+        <?php
+        for($i=0;$i<8;$i++){
+          echo "<td>Ore ".substr(explode("T", $json_fcst['data'][$i]['timestamp_local'])[1], 0, 5)."</td>";
+        }
+        ?>
+      </tr>
+      <tr> <td>% precipit.</td>
+        <?php
+        for($i=0;$i<8;$i++){
+          echo '<td style="background-color: '.getColorPop($json_fcst['data'][$i]['pop']).';">'.$json_fcst['data'][$i]['pop'].'%</td>';
+        }
+        ?>
+      </tr>
+      <tr> <td>Clima</td>
+        <?php
+        for($i=0;$i<8;$i++){
+          echo '<td><img width="50px" height="50px" src="https://www.weatherbit.io/static/img/icons/'.$json_fcst['data'][$i]['weather']['icon'].'.png"></td>';
+        }
+        ?>
+      </tr>
+      <tr> <td>max. Okta</td>
+        <?php
+        for($i=0;$i<8;$i++){
+          echo '<td>'.round((($json_fcst['data'][$i]['clouds_mid'])/100*8)).'</td>';
+        }
+        ?>
+      </tr>
+      <tr> <td>Vento <small>(beta)</small></td>
+        <?php
+        for($i=0;$i<8;$i++){
+          echo '<td>'.round($json_fcst['data'][$i]['wind_spd']*3.6).' km/h ('.$json_fcst['data'][$i]['wind_cdir'].')</td>';
+        }
+        ?>
+      </tr>
+    </table>
+  </div>
+</div>
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+    <table class="table table-dark">
+      <tr>
+        <td class="title" colspan="7">Ore Successive (fino a 24h)</td>
+      </tr>
+      <tr> <td>Ora</td>
+        <?php
+        for($i=8;$i<24;$i+=3){
+          echo "<td>Ore ".substr(explode("T", $json_fcst['data'][$i]['timestamp_local'])[1], 0, 5)."</td>";
+        }
+        ?>
+      </tr>
+      <tr> <td>% precipit.</td>
+        <?php
+        for($i=8;$i<24;$i+=3){
+          echo '<td style="background-color: '.getColorPop($json_fcst['data'][$i]['pop']).';">'.$json_fcst['data'][$i]['pop'].'%</td>';
+        }
+        ?>
+      </tr>
+      <tr> <td>Clima</td>
+        <?php
+        for($i=8;$i<24;$i+=3){
+          echo '<td><img width="50px" height="50px" src="https://www.weatherbit.io/static/img/icons/'.$json_fcst['data'][$i]['weather']['icon'].'.png"></td>';
+        }
+        ?>
+      </tr>
+      <tr> <td>max. Okta</td>
+        <?php
+        for($i=8;$i<24;$i+=3){
+          echo '<td>'.round((($json_fcst['data'][$i]['clouds_mid'])/100*8)).'</td>';
+        }
+        ?>
+      </tr>
+      <tr> <td>Visibilit&agrave; <small>(beta)</small></td>
+        <?php
+        for($i=8;$i<24;$i+=3){
+          echo '<td>'.round(($json_fcst['data'][$i]['vis'])).' km</td>';
+        }
+        ?>
+      </tr>
+      <tr> <td>Massima Umidit&agrave; R. <small>(beta)</small></td>
+        <?php
+        for($i=8;$i<24;$i+=3){
+          echo '<td>'.($json_fcst['data'][$i]['rh']).'% ';
+          if (isset($json_fcst['data'][$i-1]['rh']) && ($json_fcst['data'][$i-1]['rh'] > $json_fcst['data'][$i]['rh'])){
+            echo '<i class="fa fa-arrow-down"></i>';
+          }
+          else {
+            echo '<i class="fa fa-arrow-up"></i>';
+          }
+          echo '</td>';
+        }
+        ?>
+      </tr>
+    </table>
+  </div>
+</div>
+</div>
+
+<?php
+
+if ($RECORD_ON_DATABASE){
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, 'currentwx');
+
+  $data = [$obstime, $temp, $dewpoint, $hi, $humidity, $winddir, $wspd, $gusts, $beaufort, $press, $qnh, $uv, $wm2, $o3, $pm25, $pm10, $no2, $co, $mmh, $sw];
+  foreach ($data as $datum){
+    if ($datum=='' || is_null($datum)){
+      $datum = NULL;
+    }
+  }
+
+  $sql = "INSERT INTO weather (instant, temp, dewp, hi, rh, dirVen, minVen, maxVen, beauf, qfe, qnh, uv, whmq, o3, pm25, pm10, no2, co, mmh, sw) VALUES ('$obstime', $temp, $dewpoint, $hi, $humidity, $winddir, $wspd, $gusts, $beaufort, $press, $qnh, $uv, $wm2, $o3, $pm25, $pm10, $no2, $co, NULL, '$sw')";
+
+  $conn->query($sql);
+}
+
+
+?>
 
 </body>
 <script src="script.js" charset="utf-8"></script>
